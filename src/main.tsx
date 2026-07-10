2ef5c5739fb49302b3bf05c762fd5acf3aae2891
izalinda38 <torresizadora935@gmail.com>
2026-07-10 09:05:04 -0300

    Add global styles and update main.tsx with Tailwind directives

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { FleetProvider } from "./app/context/FleetContext.tsx";
 import "leaflet/dist/leaflet.css";
 import "./styles/design-system.css";
 import "./styles/index.css";
+import "./styles/globals.css";
 
 createRoot(document.getElementById("root")!).render(
   <FleetProvider>
